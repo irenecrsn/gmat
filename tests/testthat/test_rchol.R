615bf653465df732593f1f14aae01eec7e96aa47
irenecrsn <icordoba@fi.upm.es>
2018-11-02 11:41:00 +0100

    fixed tests

--- tests/testthat/test_rchol.R
+++ tests/testthat/test_rchol.R
@@ -1,29 +1,29 @@
-context("Private utility functions")
+context("Utility functions")
 
 test_that("random dags are actually dags", {
 	p <- 10
 
-	dag <- .rgraph(p, 0.5, dag = TRUE)
+	dag <- rgraph(p, 0.5, dag = TRUE)
 	expect_true(is_dag(dag))
 })
 
 test_that("random dags follow the natural ancestral order", {
 	p <- 10
 	
-	dag <- .rgraph(p, 0.5, dag = TRUE)
-	dag_am <- as_adjacency_matrix(dag)
+	dag <- rgraph(p, 0.5, dag = TRUE)
+	dag_am <- igraph::as_adjacency_matrix(dag)
 
-	expect_true(isTriangular(dag_am, upper = TRUE))
+	expect_true(Matrix::isTriangular(dag_am, upper = TRUE))
 })
 
 test_that("random ugs are actually ugs", {
 	p <- 10
 
-	ug <- .rgraph(p, 0.5)
-	ug_am <- as_adjacency_matrix(ug)
+	ug <- rgraph(p, 0.5)
+	ug_am <- igraph::as_adjacency_matrix(ug)
 
-	expect_true(isSymmetric(ug_am))
-	expect_identical(diag(ug_am), rep(0, p))
+	expect_true(Matrix::isSymmetric(ug_am))
+	expect_identical(sum(Matrix::diag(ug_am)), 0)
 })
 
 
