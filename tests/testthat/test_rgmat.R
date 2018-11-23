01a355166fd900fb129d849b3e840b9a901a0143
irenecrsn <icordoba@fi.upm.es>
2018-11-23 12:33:00 +0100

    updated test for rgmat functions. some have crashed

--- tests/testthat/test_rgmat.R
+++ tests/testthat/test_rgmat.R
@@ -1,16 +1,7 @@
-context("Sampling of matrices constrained by undirected graphs")
-
-test_that("the condition number is controlled", {
-	N <- 20; p <- 10; d <- 0.15; k <- 5;
-
-	sample <- diagdom(N = N, p = p, d = d, k = k)
-	for (n in 1:N) {
-		expect_equal(kappa(sample[, , n], exact = TRUE), k)
-	}
-})
+context("SPD matrices, possibly with a zero pattern")
 
 test_that("selective gram schmidt actually selects", {
-	p <- 10; d <- 0.15;
+	p <- 5; d <- 0.25;
 
 	span <- matrix(ncol = p, nrow = p, stats::rnorm(p^2))
 	
@@ -33,26 +24,66 @@ test_that("selective gram schmidt actually selects", {
 })
 
 test_that("the size of the sample is correct", {
-	N <- 20; p <- 10; d <- 0.15;
-	
-	sample <- port(N = N, p = p, d = d)
+	N <- 10; p <- 5; d <- 0.25;
+
+	# no zeros
+	sample <- port(N = N, p = p)
+	expect_equal(dim(sample)[3], N)
+	sample <- diagdom(N = N, p = p)
 	expect_equal(dim(sample)[3], N)
 
+	# with a percentage of zeros
+	sample <- port(N = N, p = p, d = d)
+	expect_equal(dim(sample)[3], N)
 	sample <- diagdom(N = N, p = p, d = d)
+	expect_equal(dim(sample)[3], N)
+
+	# with a predefined pattern of zeros
+	ug <- rgraph(p = p, d = d)
+	sample <- port(N = N, ug = ug)
+	expect_equal(dim(sample)[3], N)
+	sample <- diagdom(N = N, ug = ug)
 	expect_equal(dim(sample)[3], N)
 })
 
 test_that("matrices are symmetric positive definite", {
-	N <- 20; p <- 10; d <- 0.15;
+	N <- 10; p <- 5; d <- 0.25;
+	
+	# no zeros
+	sample <- port(N = N, p = p) 
+	for (n in 1:N) {
+		expect_equal(sample[, , n], t(sample[, , n]))
+		# here we do not test positive definiteness since 
+		# sometimes condition numbers are very high
+	}
+	sample <- diagdom(N = N, p = p)
+	for (n in 1:N) {
+		expect_equal(sample[, , n], t(sample[, , n]))
+		expect_gt(min(eigen(sample[, , n])$values), 0)
+	}
 
+	# with a percentage of zeros
 	sample <- port(N = N, p = p, d = d) 
 	for (n in 1:N) {
 		expect_equal(sample[, , n], t(sample[, , n]))
 		# here we do not test positive definiteness since 
 		# sometimes condition numbers are very high
 	}
-
 	sample <- diagdom(N = N, p = p, d = d)
+	for (n in 1:N) {
+		expect_equal(sample[, , n], t(sample[, , n]))
+		expect_gt(min(eigen(sample[, , n])$values), 0)
+	}
+	
+	# with a predefined zero pattern 
+	ug <- rgraph(p = p, d = d)
+	sample <- port(N = N, ug = ug) 
+	for (n in 1:N) {
+		expect_equal(sample[, , n], t(sample[, , n]))
+		# here we do not test positive definiteness since 
+		# sometimes condition numbers are very high
+	}
+	sample <- diagdom(N = N, ug = ug)
 	for (n in 1:N) {
 		expect_equal(sample[, , n], t(sample[, , n]))
 		expect_gt(min(eigen(sample[, , n])$values), 0)
@@ -60,7 +91,7 @@ test_that("matrices are symmetric positive definite", {
 })
 
 test_that("the graph structure is preserved", {
-	N <- 20; p <- 10; d <- 0.15;
+	N <- 10; p <- 5; d <- 0.25;
 
 	expect_equal_ug <- function(m, ug) {
 		madj <- igraph::as_adjacency_matrix(ug, sparse = FALSE)
@@ -69,16 +100,37 @@ test_that("the graph structure is preserved", {
 		expect_equal(length(which((madj - madj_learned) != 0)), 0)
 	}
 
-	ug <- igraph::sample_gnp(n = p, p = d)
+	ug <- rgraph(p = p, d = d)
 
-	sample <- port(N = N, p = p, ug = ug, d = d)
+	sample <- port(N = N, ug = ug)
 	for (n in 1:N) {
 		expect_equal_ug(m = sample[, , n], ug = ug)
 	}
 
-	sample <- diagdom(N = N, p = p, ug = ug, d = d)
+	sample <- diagdom(N = N, ug = ug)
 	for (n in 1:N) {
 		expect_equal_ug(m = sample[, , n], ug = ug)
+	}
+})
+
+test_that("the condition number is controlled", {
+	N <- 10; p <- 5; d <- 0.25; k <- 5;
+
+	# no zeros
+	sample <- diagdom(N = N, p = p, k = k)
+	for (n in 1:N) {
+		expect_equal(kappa(sample[, , n], exact = TRUE), k)
+	}
+	# with a percentage of zeros
+	sample <- diagdom(N = N, p = p, d = d, k = k)
+	for (n in 1:N) {
+		expect_equal(kappa(sample[, , n], exact = TRUE), k)
+	}
+	# with a predefined zero pattern 
+	ug <- rgraph(p = p, d = d)
+	sample <- diagdom(N = N, ug = ug, k = k)
+	for (n in 1:N) {
+		expect_equal(kappa(sample[, , n], exact = TRUE), k)
 	}
 })
 
