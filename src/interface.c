30d7db0539c76f99ee3537974a1bd5c6ea2c284b
irenecrsn <icordoba@fi.upm.es>
2019-09-25 12:56:10 +0200

    Correct spacing

--- src/interface.c
+++ src/interface.c
@@ -2,15 +2,15 @@
 #include "gmn_sampling.h"
 
 SEXP C_gram_schmidt_sel (SEXP madj, SEXP mcov) {
-    int n_col = ncols(madj);
-    SEXP mort;
+	int n_col = ncols(madj);
+	SEXP mort;
 	double *p_mort;
 
-    mort = PROTECT(allocMatrix(REALSXP, n_col, n_col));
+	mort = PROTECT(allocMatrix(REALSXP, n_col, n_col));
 	p_mort = REAL(mort);
     
 	gram_schmidt_sel(p_mort, REAL(madj), REAL(mcov), n_col);
     
-    UNPROTECT(1);
-    return mort;
+	UNPROTECT(1);
+	return mort;
 }
