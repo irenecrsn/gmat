0af7c2b63498704594cacfaab55e20920078f582
irenecrsn <icordoba@fi.upm.es>
2019-09-30 11:26:42 +0200

    Simplify memory management

--- src/port.c
+++ src/port.c
@@ -8,66 +8,46 @@
 #include "port.h"
 #include "error.h"
 
-static int gram_schmidt (double **span_ort, double **span,
-		const unsigned int nvec, const unsigned int dim);
+static int gram_schmidt (double *span_ort, double **span,
+		const unsigned int nvec, const unsigned int p);
 
 static void proj_ort (double *v_proj_u, const double *v,
-		const double *u, const unsigned int dim);
+		const double *u, const unsigned int p);
 
 /*
  * Selective Gram Schmidt algorithm
  */
 int gram_schmidt_sel (double *mort, const double *madj,
-		const double *mcov, const unsigned int dim) {
+		const double *mcov, const unsigned int p) {
 
-	double **span_sel = NULL, **ort_base = NULL;
-	double *v_proj = NULL;
+	double *v_proj = NULL, *ort_base = NULL, **span_sel = NULL;
 	unsigned int i = 0, j = 0;
 	unsigned int n_span = 0, j_current = 0;
-
-	#define FREEALL \
-	for (j = 0; j < i; j++) {\
-		free(ort_base[j]); ort_base[j] = NULL;\
-	}\
-	free(v_proj); v_proj = NULL;\
-	free(span_sel); span_sel = NULL;\
-	free(ort_base); ort_base = NULL;
 
 	if (mort == NULL || madj == NULL || mcov == NULL) {
 		return GMAT_ENULL;
 	}
 
-	if ((v_proj = calloc(dim, sizeof(double))) == NULL) {
+	v_proj = calloc(p, sizeof(double));
+	span_sel = calloc(p, sizeof(double *));
+	ort_base = calloc(p * p, sizeof(double));
+
+	if (v_proj == NULL || span_sel == NULL || ort_base == NULL) {
+		free(v_proj); v_proj = NULL;
+		free(span_sel); span_sel = NULL;
+		free(ort_base); ort_base = NULL;
 		return GMAT_ENOMEM;
 	}
 
-	if ((span_sel = calloc(dim, sizeof(double *))) == NULL) {
-		FREEALL;
-		return GMAT_ENOMEM;
-	}
+	for (j = 0; j < p; j++) {
 
-	if ((ort_base = calloc(dim, sizeof(double *))) == NULL) {
-		FREEALL;
-		return GMAT_ENOMEM;
-	}
-
-	for (i = 0; i < dim; i++) {
-		ort_base[i] = NULL;
-		if ((ort_base[i] = calloc(dim, sizeof(double))) == NULL) {
-			FREEALL;
-			return GMAT_ENOMEM;
-		}
-	}
-
-	for (j = 0; j < dim; j++) {
-
-		j_current = j * dim;
-		memcpy(mort + j_current, mcov + j_current, sizeof(double) * dim);
+		j_current = j * p;
+		memcpy(mort + j_current, mcov + j_current, sizeof(double) * p);
 		n_span = 0;
 
 		for (i = 0; i < j; i++) {
 			if (madj[j_current + i] == 0) {
-				span_sel[n_span] = mort + i * dim;
+				span_sel[n_span] = mort + i * p;
 				n_span++;
 			}
 		}
@@ -75,13 +55,15 @@ int gram_schmidt_sel (double *mort, const double *madj,
 		n_span++;
 
 		/* we orthonormalize the span obtained for the current row */
-		gram_schmidt(ort_base, span_sel, n_span, dim);
-		for (i = 0; i < dim; i++) {
-			mort[j_current + i] = ort_base[n_span - 1][i];
+		gram_schmidt(ort_base, span_sel, n_span, p);
+		for (i = 0; i < p; i++) {
+			mort[j_current + i] = ort_base[(n_span - 1)*p + i];
 		}
 	}
 
-	FREEALL;
+	free(v_proj); v_proj = NULL;
+	free(span_sel); span_sel = NULL;
+	free(ort_base); ort_base = NULL;
 	return GMAT_OK;
 }
 
@@ -129,40 +111,41 @@ int crossproduct (double * res, const double *mort, const double *madj,
  * @param span_ort Matrix with rows containing the orthogonal vectors
  * @param span Matrix with rows containing the vectors to orthogonalize
  * @param nvec Number of vectors to orthogonalize (rows of span)
- * @param dim Dimension of vectors (columns of span)
+ * @param p Dimension of vectors (columns of span)
  */
-static int gram_schmidt (double **span_ort, double **span,
-		const unsigned int nvec, const unsigned int dim)
+static int gram_schmidt (double *span_ort, double **span,
+		const unsigned int nvec, const unsigned int p)
 {
 	double *v_proj = NULL;
-	unsigned int i = 0, j = 0, k = 0;
+	unsigned int i = 0, j = 0, k = 0, i_current = 0;
 	double norm = 0;
 
 	assert(span_ort != NULL); assert(span != NULL); assert(nvec != NULL);
 
 	for (i = 0; i < nvec; i++) {
-		memcpy(span_ort[i], span[i], sizeof(double) * dim);
+		memcpy(span_ort + i*p, span[i], sizeof(double) * p);
 	}
 
-	if ((v_proj = calloc(dim, sizeof(double))) == NULL) {
+	if ((v_proj = calloc(p, sizeof(double))) == NULL) {
 		return GMAT_ENOMEM;
 	}
 
 	for (i = 0; i < nvec; i++) {
+		i_current = i * p;
 		for (j = 0; j < i; j++) {
-			proj_ort(v_proj, span_ort[i], span_ort[j], dim);
-			for (k = 0; k < dim; k++) {
-				span_ort[i][k] -= v_proj[k];
+			proj_ort(v_proj, span_ort + i_current, span_ort + j*p, p);
+			for (k = 0; k < p; k++) {
+				span_ort[i_current + k] -= v_proj[k];
 			}
 		}
 		/* we normalize the resulting vector */
 		norm = 0;
-		for (k = 0; k < dim; k++) {
-			norm += span_ort[i][k] * span_ort[i][k];
+		for (k = 0; k < p; k++) {
+			norm += span_ort[i_current + k] * span_ort[i_current + k];
 		}
 		norm = 1 / sqrt(norm);
-		for (k = 0; k < dim; k++) {
-			span_ort[i][k] = span_ort[i][k] * norm;
+		for (k = 0; k < p; k++) {
+			span_ort[i_current + k] = span_ort[i_current + k] * norm;
 		}
 	}
 
@@ -176,7 +159,7 @@ static int gram_schmidt (double **span_ort, double **span,
  * Vector u is assumed to already be normalized.
  */
 static void proj_ort (double *v_proj_u, const double *v, const double *u,
-		const unsigned int dim)
+		const unsigned int p)
 {
 	unsigned int i = 0;
 	double dot_uv = 0;
@@ -184,11 +167,11 @@ static void proj_ort (double *v_proj_u, const double *v, const double *u,
 	assert(u != NULL); assert(v != NULL);
 	assert(v_proj_u != NULL);
 
-	for (i = 0; i < dim; i++) {
+	for (i = 0; i < p; i++) {
 		dot_uv += (u[i] * v[i]);
 	}
 
-	for (i = 0; i < dim; i++) {
+	for (i = 0; i < p; i++) {
 		v_proj_u[i] = dot_uv * u[i];
 	}
 }
