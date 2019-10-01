1c2fa9e6198e4781b7c0ffeeb301d3d2a3972648
irenecrsn <icordoba@fi.upm.es>
2019-10-01 13:42:44 +0200

    Simplify R-C interface

--- src/interface.c
+++ src/interface.c
@@ -6,7 +6,6 @@ SEXP C_port (SEXP R_madj, SEXP R_Q) {
 	int *dims = NULL;
 	unsigned int N = 0, p = 0;
 	SEXP R_res = NULL, R_dims = NULL;
-	double *madj, *Q, *res;
 	port_errno_t port_errno;
 
 	/* Initialize problem dimensions */
@@ -21,14 +20,10 @@ SEXP C_port (SEXP R_madj, SEXP R_Q) {
 	}
 	p = dims[0]; N = dims[2];
 
-	/* Initialize R objects */
+	/* Initialize result */
 	R_res = PROTECT(alloc3DArray(REALSXP, p, p, N));
-	res = REAL(R_res);
 
-	/* Get argument pointers */
-	madj = REAL(R_madj); Q = REAL(R_Q);
-
-	port_errno = port_sample(res, madj, Q, p, N);
+	port_errno = port_sample(REAL(R_res), REAL(R_madj), REAL(R_Q), p, N);
 	if (port_errno != PORT_OK) {
 		UNPROTECT(1);
 		error("%s.\n", port_strerror(port_errno));
